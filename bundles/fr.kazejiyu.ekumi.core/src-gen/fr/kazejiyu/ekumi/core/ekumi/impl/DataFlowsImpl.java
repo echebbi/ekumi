49fb25a01c1e35386a69b03eda9194655b4c0116
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-05-18 15:01:15 +0200

    Make DataFlows able to resolve inputs value
    Update an activity's inputs based on the value of its predecessors' outputs.

--- bundles/fr.kazejiyu.ekumi.core/src-gen/fr/kazejiyu/ekumi/core/ekumi/impl/DataFlowsImpl.java
+++ bundles/fr.kazejiyu.ekumi.core/src-gen/fr/kazejiyu/ekumi/core/ekumi/impl/DataFlowsImpl.java
@@ -2,12 +2,15 @@
  */
 package fr.kazejiyu.ekumi.core.ekumi.impl;
 
+import fr.kazejiyu.ekumi.core.ekumi.Activity;
 import fr.kazejiyu.ekumi.core.ekumi.DataFlow;
 import fr.kazejiyu.ekumi.core.ekumi.DataFlows;
 import fr.kazejiyu.ekumi.core.ekumi.EkumiPackage;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 
 import org.eclipse.emf.common.util.EList;
@@ -15,9 +18,11 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
 
 import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.util.InternalEList;
 
 /**
@@ -28,22 +33,23 @@ import org.eclipse.emf.ecore.util.InternalEList;
  * The following features are implemented:
  * </p>
  * <ul>
- *   <li>{@link fr.kazejiyu.ekumi.core.ekumi.impl.DataFlowsImpl#getIncoming <em>Incoming</em>}</li>
+ *   <li>{@link fr.kazejiyu.ekumi.core.ekumi.impl.DataFlowsImpl#getIncomings <em>Incomings</em>}</li>
  *   <li>{@link fr.kazejiyu.ekumi.core.ekumi.impl.DataFlowsImpl#getOutgoings <em>Outgoings</em>}</li>
+ *   <li>{@link fr.kazejiyu.ekumi.core.ekumi.impl.DataFlowsImpl#getOwner <em>Owner</em>}</li>
  * </ul>
  *
  * @generated
  */
 public class DataFlowsImpl extends MinimalEObjectImpl.Container implements DataFlows {
 	/**
-	 * The cached value of the '{@link #getIncoming() <em>Incoming</em>}' containment reference list.
+	 * The cached value of the '{@link #getIncomings() <em>Incomings</em>}' containment reference list.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @see #getIncoming()
+	 * @see #getIncomings()
 	 * @generated
 	 * @ordered
 	 */
-	protected EList<DataFlow> incoming;
+	protected EList<DataFlow> incomings;
 
 	/**
 	 * The cached value of the '{@link #getOutgoings() <em>Outgoings</em>}' containment reference list.
@@ -79,11 +85,11 @@ public class DataFlowsImpl extends MinimalEObjectImpl.Container implements DataF
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	public EList<DataFlow> getIncoming() {
-		if (incoming == null) {
-			incoming = new EObjectContainmentEList<DataFlow>(DataFlow.class, this, EkumiPackage.DATA_FLOWS__INCOMING);
+	public EList<DataFlow> getIncomings() {
+		if (incomings == null) {
+			incomings = new EObjectContainmentEList<DataFlow>(DataFlow.class, this, EkumiPackage.DATA_FLOWS__INCOMINGS);
 		}
-		return incoming;
+		return incomings;
 	}
 
 	/**
@@ -103,13 +109,88 @@ public class DataFlowsImpl extends MinimalEObjectImpl.Container implements DataF
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	public Activity getOwner() {
+		if (eContainerFeatureID() != EkumiPackage.DATA_FLOWS__OWNER)
+			return null;
+		return (Activity) eInternalContainer();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetOwner(Activity newOwner, NotificationChain msgs) {
+		msgs = eBasicSetContainer((InternalEObject) newOwner, EkumiPackage.DATA_FLOWS__OWNER, msgs);
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setOwner(Activity newOwner) {
+		if (newOwner != eInternalContainer()
+				|| (eContainerFeatureID() != EkumiPackage.DATA_FLOWS__OWNER && newOwner != null)) {
+			if (EcoreUtil.isAncestor(this, newOwner))
+				throw new IllegalArgumentException("Recursive containment not allowed for " + toString());
+			NotificationChain msgs = null;
+			if (eInternalContainer() != null)
+				msgs = eBasicRemoveFromContainer(msgs);
+			if (newOwner != null)
+				msgs = ((InternalEObject) newOwner).eInverseAdd(this, EkumiPackage.ACTIVITY__FLOWS, Activity.class,
+						msgs);
+			msgs = basicSetOwner(newOwner, msgs);
+			if (msgs != null)
+				msgs.dispatch();
+		} else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, EkumiPackage.DATA_FLOWS__OWNER, newOwner, newOwner));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * {@inheritDoc}
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	@Override
+	public void resolveInputs() {
+		for (DataFlow flow : getIncomings()) {
+			flow.getOutput().setValue(flow.getInput().getValue());
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseAdd(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+		case EkumiPackage.DATA_FLOWS__OWNER:
+			if (eInternalContainer() != null)
+				msgs = eBasicRemoveFromContainer(msgs);
+			return basicSetOwner((Activity) otherEnd, msgs);
+		}
+		return super.eInverseAdd(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
 	@Override
 	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
 		switch (featureID) {
-		case EkumiPackage.DATA_FLOWS__INCOMING:
-			return ((InternalEList<?>) getIncoming()).basicRemove(otherEnd, msgs);
+		case EkumiPackage.DATA_FLOWS__INCOMINGS:
+			return ((InternalEList<?>) getIncomings()).basicRemove(otherEnd, msgs);
 		case EkumiPackage.DATA_FLOWS__OUTGOINGS:
 			return ((InternalEList<?>) getOutgoings()).basicRemove(otherEnd, msgs);
+		case EkumiPackage.DATA_FLOWS__OWNER:
+			return basicSetOwner(null, msgs);
 		}
 		return super.eInverseRemove(otherEnd, featureID, msgs);
 	}
@@ -120,12 +201,28 @@ public class DataFlowsImpl extends MinimalEObjectImpl.Container implements DataF
 	 * @generated
 	 */
 	@Override
+	public NotificationChain eBasicRemoveFromContainerFeature(NotificationChain msgs) {
+		switch (eContainerFeatureID()) {
+		case EkumiPackage.DATA_FLOWS__OWNER:
+			return eInternalContainer().eInverseRemove(this, EkumiPackage.ACTIVITY__FLOWS, Activity.class, msgs);
+		}
+		return super.eBasicRemoveFromContainerFeature(msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
-		case EkumiPackage.DATA_FLOWS__INCOMING:
-			return getIncoming();
+		case EkumiPackage.DATA_FLOWS__INCOMINGS:
+			return getIncomings();
 		case EkumiPackage.DATA_FLOWS__OUTGOINGS:
 			return getOutgoings();
+		case EkumiPackage.DATA_FLOWS__OWNER:
+			return getOwner();
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
@@ -139,13 +236,16 @@ public class DataFlowsImpl extends MinimalEObjectImpl.Container implements DataF
 	@Override
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
-		case EkumiPackage.DATA_FLOWS__INCOMING:
-			getIncoming().clear();
-			getIncoming().addAll((Collection<? extends DataFlow>) newValue);
+		case EkumiPackage.DATA_FLOWS__INCOMINGS:
+			getIncomings().clear();
+			getIncomings().addAll((Collection<? extends DataFlow>) newValue);
 			return;
 		case EkumiPackage.DATA_FLOWS__OUTGOINGS:
 			getOutgoings().clear();
 			getOutgoings().addAll((Collection<? extends DataFlow>) newValue);
+			return;
+		case EkumiPackage.DATA_FLOWS__OWNER:
+			setOwner((Activity) newValue);
 			return;
 		}
 		super.eSet(featureID, newValue);
@@ -159,11 +259,14 @@ public class DataFlowsImpl extends MinimalEObjectImpl.Container implements DataF
 	@Override
 	public void eUnset(int featureID) {
 		switch (featureID) {
-		case EkumiPackage.DATA_FLOWS__INCOMING:
-			getIncoming().clear();
+		case EkumiPackage.DATA_FLOWS__INCOMINGS:
+			getIncomings().clear();
 			return;
 		case EkumiPackage.DATA_FLOWS__OUTGOINGS:
 			getOutgoings().clear();
+			return;
+		case EkumiPackage.DATA_FLOWS__OWNER:
+			setOwner((Activity) null);
 			return;
 		}
 		super.eUnset(featureID);
@@ -177,12 +280,29 @@ public class DataFlowsImpl extends MinimalEObjectImpl.Container implements DataF
 	@Override
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
-		case EkumiPackage.DATA_FLOWS__INCOMING:
-			return incoming != null && !incoming.isEmpty();
+		case EkumiPackage.DATA_FLOWS__INCOMINGS:
+			return incomings != null && !incomings.isEmpty();
 		case EkumiPackage.DATA_FLOWS__OUTGOINGS:
 			return outgoings != null && !outgoings.isEmpty();
+		case EkumiPackage.DATA_FLOWS__OWNER:
+			return getOwner() != null;
 		}
 		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eInvoke(int operationID, EList<?> arguments) throws InvocationTargetException {
+		switch (operationID) {
+		case EkumiPackage.DATA_FLOWS___RESOLVE_INPUTS:
+			resolveInputs();
+			return null;
+		}
+		return super.eInvoke(operationID, arguments);
 	}
 
 } //DataFlowsImpl
