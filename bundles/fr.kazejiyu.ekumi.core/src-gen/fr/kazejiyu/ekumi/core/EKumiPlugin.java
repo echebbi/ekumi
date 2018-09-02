b24f239159893211d8025b1d301b733032685e1b
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-09-02 02:56:19 +0200

    Add an activator to the core bundle
    This activator provides utilities related to the plug-in:
    - id,
    - state location under .metadata/.plugins/

---
+++ bundles/fr.kazejiyu.ekumi.core/src-gen/fr/kazejiyu/ekumi/core/EKumiPlugin.java
@@ -0,0 +1,71 @@
+package fr.kazejiyu.ekumi.core;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.emf.common.util.URI;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import fr.kazejiyu.ekumi.core.exceptions.EKumiRuntimeException;
+
+/**
+ * Activator of the {@code fr.kazejiyu.ekumi.core} bundle.
+ * 
+ * @author Emmanuel CHEBBI
+ */
+public class EKumiPlugin implements BundleActivator {
+	
+	public static final String ID = "fr.kazejiyu.ekumi.core";
+	
+	private static final URI STATE_LOCATION_URI = URI.createURI("platform:/meta/" + ID + "/");
+
+	// The shared instance
+	private static EKumiPlugin plugin;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		plugin = this;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static EKumiPlugin getDefault() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns the URI of the state directory for this bundle under <i>.metadata/.plugins/</i>.
+	 * @return the URI of the state directory for this bundle under <i>.metadata/.plugins/</i>
+	 */
+	public static URI getStateLocationURI() {
+		return STATE_LOCATION_URI;
+	}
+	
+	/**
+	 * Returns the location of the state directory for this bundle under <i>.metadata/.plugins/</i>.
+	 * @return the location of the state directory for this bundle under <i>.metadata/.plugins/</i>
+	 */
+	public static Path getStateLocation() {
+		try {
+			return Paths.get(FileLocator.resolve(new URL(getStateLocationURI().toString())).toURI());
+			
+		} catch (URISyntaxException | IOException e) {
+			// Should never happen
+			throw new EKumiRuntimeException("An unexpected error occurred while resolving the state location of " + ID, e);
+		}
+	}
+
+}
