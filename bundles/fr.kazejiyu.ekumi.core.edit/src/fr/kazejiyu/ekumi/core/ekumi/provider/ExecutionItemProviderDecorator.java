ed1ea96cbe423c988a5f4b51cfaf98e0a600a17b
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-09-04 23:52:38 +0200

    Implement custom JFace item providers
    The new providers make possible to customize the way
    EKumi objects are displayed in JFace viewers.

---
+++ bundles/fr.kazejiyu.ekumi.core.edit/src/fr/kazejiyu/ekumi/core/ekumi/provider/ExecutionItemProviderDecorator.java
@@ -0,0 +1,22 @@
+package fr.kazejiyu.ekumi.core.ekumi.provider;
+
+import fr.kazejiyu.ekumi.core.ekumi.Execution;
+
+/**
+ * A custom item provider dedicated to {@link Execution} instances.
+ * 
+ * @author Emmanuel CHEBBI
+ */
+public class ExecutionItemProviderDecorator extends ItemProviderAdapterDecorator {
+	
+	public ExecutionItemProviderDecorator(EkumiDecoratorAdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	@Override
+	public String getText(Object object) {
+		Execution execution = (Execution) object;
+		return execution.getActivity().getId();
+	}
+
+}
