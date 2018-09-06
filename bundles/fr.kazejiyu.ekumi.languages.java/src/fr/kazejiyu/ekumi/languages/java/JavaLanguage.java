729d40898539e9ef6f9c353164c2272306a38db6
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2018-09-06 21:10:38 +0200

    Remove the IProject parameter from ScriptingLanguage's methods
    Since an IProject instance represents a project that exists within
    Eclipse IDE's workspace, keeping this parameter would have preventing
    EKumi from being used out of Eclipse IDE in the future.

--- bundles/fr.kazejiyu.ekumi.languages.java/src/fr/kazejiyu/ekumi/languages/java/JavaLanguage.java
+++ bundles/fr.kazejiyu.ekumi.languages.java/src/fr/kazejiyu/ekumi/languages/java/JavaLanguage.java
@@ -7,12 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.launching.JavaRuntime;
+import org.osgi.framework.Bundle;
 
 import fr.kazejiyu.ekumi.core.ekumi.Condition;
 import fr.kazejiyu.ekumi.core.ekumi.Runner;
@@ -21,31 +24,81 @@ import fr.kazejiyu.ekumi.core.languages.ScriptingLanguage;
 import fr.kazejiyu.ekumi.core.languages.exceptions.ScriptLoadingFailureException;
 
 /**
- * Loads {@link Script}s written in Java.
+ * Loads {@link Script}s written in Java.<br>
+ * <br>
+ * In order to resolve the script, an instance of this class expects a unique String identifier.
+ * This identifier must be the fully qualified name of the script's class next to the if of its project or bundle,
+ * the two separated by a semicolon.<br>
+ * <br>
+ * For instance, the identifier corresponding to the class {@code com.domain.MyClass} within the {@code com.domain}
+ * bundle must be:
+ * <ul>
+ * 	<li>com.domain;com.domain.MyClass
+ * </ul>
+ * Moreover, the script class must implement either {@link Runner} or {@link Condition} and must provide a
+ * default, nullary constructor.
+ * 
+ * FIXME [Refactor] With the current implementation, any workspace project owning the same name as a bundle
+ * 					will be hidden by the bundle, which makes impossible to resolve a class located in the project. 
  * 
  * @author Emmanuel CHEBBI
  */
-public class JavaLanguage implements ScriptingLanguage {
+public final class JavaLanguage implements ScriptingLanguage {
 
 	@Override
-	public Runner resolveRunner(String path, IProject project) {
+	public Runner resolveRunner(String identifier) {
 		try {
-			Class<?> clazz = loadClass(path, project);
-			return clazz.asSubclass(Runner.class).newInstance();
+			return resolve(identifier, Runner.class);
 			
-		} catch (InstantiationException | IllegalAccessException e) {
-			throw new ScriptLoadingFailureException("Unable to instantiate a Runner from path: " + path, e); 
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			throw new ScriptLoadingFailureException("Unable to instantiate a Runner from path: " + identifier, e); 
 		}
 	}
 
 	@Override
-	public Condition resolveCondition(String path, IProject project) {
+	public Condition resolveCondition(String identifier) {
 		try {
-			Class<?> clazz = loadClass(path, project);
-			return clazz.asSubclass(Condition.class).newInstance();
+			return resolve(identifier, Condition.class);
 			
-		} catch (InstantiationException | IllegalAccessException e) {
-			throw new ScriptLoadingFailureException("Unable to instantiate a Condition from path: " + path, e); 
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			throw new ScriptLoadingFailureException("Unable to instantiate a Condition from path: " + identifier, e); 
+		}
+	}
+	
+	/**
+	 * Resolves a specific {@link Script} class and instantiates it.
+	 * 
+	 * @param identifier
+	 * 			The String identifying the script to resolve.
+	 * @param clazz
+	 * 			The expected class of the Script.
+	 * 
+	 * @return a new instance of {@code clazz}.
+	 * 
+	 * @param <T> The expected type of the script to instantiate. 
+	 * 
+	 * @throws InstantiationException if this Class represents an abstract class,an interface, 
+	 * 								  a array class, a primitive type, or void;or if the class 
+	 * 								  has no nullary constructor;or if the instantiation fails 
+	 * 								  for some other reason.
+	 * @throws IllegalAccessException if the class or its nullary constructor is not accessible.
+	 * @throws ClassNotFoundException if the class cannot be found.
+	 * @throws ClassCastException if the new script instance cannot be casted to the given clazz.
+	 */
+	private static <T> T resolve(String identifier, Class<T> clazz) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		String[] pluginAndClass = identifier.split(";");
+		String fullyQualifiedName = pluginAndClass[1];
+		String plugin = pluginAndClass[0];
+
+		Bundle bundle = Platform.getBundle(plugin);
+		
+		// Bundle  == plug-in that is installed within Eclipse IDE
+		// IProject == project located within the workspace
+		if (bundle != null) {
+			return bundle.loadClass(fullyQualifiedName).asSubclass(clazz).newInstance();
+		} else {
+			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(plugin); 
+			return loadFromProject(fullyQualifiedName, project).asSubclass(clazz).newInstance();
 		}
 	}
 	
@@ -61,11 +114,11 @@ public class JavaLanguage implements ScriptingLanguage {
 	 * 
 	 * @throws ScriptLoadingFailureException if an error occurs while loading the class.
 	 */
-	private Class<?> loadClass(String fullyQualifiedName, IProject project) {
+	private static Class<?> loadFromProject(String fullyQualifiedName, IProject project) {
 		try {
 			URLClassLoader loader = getClassLoader(project);
 			return loader.loadClass(fullyQualifiedName);
-			
+
 		} catch (MalformedURLException | CoreException | ClassNotFoundException | ClassCastException e) {
 			throw new ScriptLoadingFailureException("Unable to resolve any Java class from path: " + fullyQualifiedName + " in project " + project, e); 
 		}
@@ -85,7 +138,7 @@ public class JavaLanguage implements ScriptingLanguage {
 	 * @throws CoreException if project's classpath cannot be computed.
 	 * @throws MalformedURLException if a classpath's entry is invalid. Should not occur.
 	 */
-	private URLClassLoader getClassLoader(IProject project) throws CoreException, MalformedURLException {
+	private static URLClassLoader getClassLoader(IProject project) throws CoreException, MalformedURLException {
 		IJavaProject javaProject = JavaCore.create(project);
 		String[] classPathEntries = JavaRuntime.computeDefaultRuntimeClassPath(javaProject);
 		List<URL> urlList = new ArrayList<>();
@@ -96,10 +149,10 @@ public class JavaLanguage implements ScriptingLanguage {
 			URL url = path.toFile().toURI().toURL();
 			urlList.add(url);
 		}
-
+		
 		// parentClassLoader makes possible to cast instances
 		// loaded by the new URLClassLoader into EKumi classes.
-		ClassLoader parentClassLoader = getClass().getClassLoader();
+		ClassLoader parentClassLoader = JavaLanguage.class.getClassLoader();
 		URL[] urls = urlList.toArray(new URL[urlList.size()]);
 		
 		return new URLClassLoader(urls, parentClassLoader);
