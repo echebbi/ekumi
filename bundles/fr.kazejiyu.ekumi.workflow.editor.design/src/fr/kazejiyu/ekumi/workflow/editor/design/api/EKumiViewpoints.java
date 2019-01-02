ec84ae83bf805d5672ddd73f8d11a17119f46e17
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2019-01-02 01:54:02 +0100

    Expose EKumi's Sirius viewpoints
    Make viewpoints accessible thanks to the EKumiViewpoints class.

---
+++ bundles/fr.kazejiyu.ekumi.workflow.editor.design/src/fr/kazejiyu/ekumi/workflow/editor/design/api/EKumiViewpoints.java
@@ -0,0 +1,50 @@
+package fr.kazejiyu.ekumi.workflow.editor.design.api;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
+
+import fr.kazejiyu.ekumi.workflow.editor.design.Activator;
+
+/**
+ * <p>Provides access to EKumi Sirius viewpoints.</p>
+ * 
+ * <p>Currently, the following viewpoints are available:
+ * 	<ul>
+ * 		<li>{@value #WORKFLOW_NAME} -> provides representations of a workflow
+ * 	</ul>
+ * </p>
+ */
+public enum EKumiViewpoints {
+	
+	INSTANCE;
+	
+	/** The name of the 'EKumi Workflow' viewpoint. */
+	public static final String WORKFLOW_NAME = "EKumi Workflow";
+	
+	/**
+	 * Returns all the Sirius viewpoints related to EKumi.
+	 * @return all the Sirius viewpoints related to EKumi
+	 */
+	public static Set<Viewpoint> all() {
+		return new HashSet<>(Activator.getViewpoints());
+	}
+	
+	/**
+	 * <p>Returns the 'EKumi Workflow viewpoint</p>
+	 * 
+	 * <p>This viewpoint provides representations of a workflow.</p>
+	 * 
+	 * <p>Should never return an empty Optional.</p>
+	 *  
+	 * @return the viewpoint, if found
+	 */
+	public static Optional<Viewpoint> workflow() {
+		return all().stream()
+					.filter(viewpoint -> WORKFLOW_NAME.equals(viewpoint.getName()))
+					.findAny();
+	}
+
+}
