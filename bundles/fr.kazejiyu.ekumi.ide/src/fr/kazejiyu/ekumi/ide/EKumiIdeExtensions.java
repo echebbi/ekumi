7a5b602396e6b672e971f4caa619aebb2c298b2f
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2019-03-21 10:37:05 +0100

    Provide a new 'representations' attribute to the 'project_customization' extension point
    This extension point allows users to provide new ways to represent a workflow, e.g.
     - new diagram editors,
     - new textual editors

---
+++ bundles/fr.kazejiyu.ekumi.ide/src/fr/kazejiyu/ekumi/ide/EKumiIdeExtensions.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2018-2019 Emmanuel CHEBBI
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+package fr.kazejiyu.ekumi.ide;
+
+public final class EKumiIdeExtensions {
+	
+	/** ID of the 'project_customization' extension point */
+	public static final String PROJECT_CUSTOMIZATION_EXTENSION_ID = EKumiIdePlugin.ID + ".project_customization";
+	
+	private EKumiIdeExtensions() {
+		// makes no sense to instantiate it
+	}
+
+}
