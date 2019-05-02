cecc831bb00fd6dd157ecfe1da747def22ba3eb6
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2019-05-02 23:03:38 +0200

    Provide a contextual menu to execute an EDS model
    To trigger this contextual menu:
     - Right-click on the *.eds file
     - Run As > EKumi Activity

--- bundles/fr.kazejiyu.ekumi.debug.ui/src/fr/kazejiyu/ekumi/debug/ui/LaunchWorkflowFromFileShortcut.java
+++ bundles/fr.kazejiyu.ekumi.debug.ui/src/fr/kazejiyu/ekumi/debug/ui/LaunchWorkflowFromFileShortcut.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.PlatformObject;
 import org.eclipse.debug.core.DebugPlugin;
@@ -36,48 +35,38 @@ import fr.kazejiyu.ekumi.core.workflow.Activity;
 import fr.kazejiyu.ekumi.debug.EKumiRunConfiguration;
 
 /**
- * <p>Creates a new Launch Configuration for a given EKumi model.</p>
+ * <p>Creates a new Launch Configuration for a given EKumi model then runs it.</p>
  * 
- * <p>The {@link #launch(ISelection, String)} method is supposed to be called
- * from Eclipse IDE's contextual menu on an *.ekumi model file. It then creates
- * a corresponding Launch Configuration and runs it.</p>
+ * <p>This class is intended to be sub-classed by clients. It provides an easy way
+ * to add a _Run As_ shortcut on files serializing an EMF model that can be adapted
+ * to an EKumi {@link Activity}.</p>
  */
-public class LaunchWorkflowShortcut implements ILaunchShortcut {
+public abstract class LaunchWorkflowFromFileShortcut implements ILaunchShortcut {
 
 	@Override
 	public void launch(ISelection selection, String mode) {
 		URI fileURI = selectedFileURI(selection);
 		
-		if (fileURI == null)
+		if (fileURI == null) {
 			return;
-		
+		}
 		try {
-			Activity activity = loadActivity(fileURI);
-			
-			if (activity == null)
-				return;
-			
 			ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
 			ILaunchConfigurationType type = manager.getLaunchConfigurationType(EKumiRunConfiguration.ID);
 			ILaunchConfiguration[] configurations = manager.getLaunchConfigurations(type);
 			
 			// Create a new launch configuration
-			String launchConfigurationName = activity.getName();
-			int index = 1;
-			
-			while (configurationExists(configurations, launchConfigurationName)) {
-				++index;
-				launchConfigurationName = activity.getName() + " (" + index + ")";
-			}
+			String baseConfigurationName = configurationNameFor(fileURI);
+			String launchConfigurationName = availableLaunchConfigurationName(baseConfigurationName, configurations);
 			
 			ILaunchConfigurationWorkingCopy workingCopy = type.newInstance(null, launchConfigurationName);
-			workingCopy.setAttribute(EKumiRunConfiguration.EKUMI_MODEL_URI, activity.eResource().getURI().toString());
+			workingCopy.setAttribute(EKumiRunConfiguration.EKUMI_MODEL_URI, fileURI.toString());
 			ILaunchConfiguration configuration = workingCopy.doSave();
 			
 			// Run the new launch configuration
 			configuration.launch(ILaunchManager.RUN_MODE, null);
 			
-		} catch(CoreException | IOException e) {
+		} catch(Exception e) {
 			Activator.error(e, "An error occurred while launching the workflow " + fileURI);
 		}
 	}
@@ -85,6 +74,45 @@ public class LaunchWorkflowShortcut implements ILaunchShortcut {
 	@Override
 	public void launch(IEditorPart editor, String mode) {
 		// should not be called
+	}
+	
+	/**
+	 * <p>Returns the name of the new launch configuration that will be created
+	 * for the given fileURI.</p>
+	 * 
+	 * <p>Any exception thrown by this method will be properly handled.</p>
+	 * 
+	 * @param fileURI
+	 * 			The URI of the resource holding the model that is about to be executed.
+	 * 
+	 * @return the name of the new launch configuration
+	 * 
+	 * @throws Exception if an error occurs
+	 */
+	// Ignore the "Define and throw a dedicated exception instead of using a generic one." rule
+	// as "throw Exception" is used here on purpose to let client's code fail safely.
+	@SuppressWarnings("squid:S00112")
+	protected abstract String configurationNameFor(URI fileURI) throws Exception;
+
+	/**
+	 * Computes a launch configuration name that is not used by any existing configuration.
+	 * 
+	 * @param baseConfigurationName
+	 *			The name that serves as a base to compute the name of the new launch configuration.
+	 * @param configurations
+	 * 			The existing launch configurations to ensure that the computed name is unique.
+	 * 
+	 * @return a unique name based on given base name and that is not used by any existing launch configuration
+	 */
+	private static String availableLaunchConfigurationName(String baseConfigurationName, ILaunchConfiguration[] configurations) {
+		String launchConfigurationName = baseConfigurationName;
+		int index = 0;
+		
+		while (configurationExists(configurations, launchConfigurationName)) {
+			++index;
+			launchConfigurationName = baseConfigurationName + " (" + index + ")";
+		}
+		return launchConfigurationName;
 	}
 	
 	/**
@@ -115,18 +143,18 @@ public class LaunchWorkflowShortcut implements ILaunchShortcut {
 	 * @return the URI of the selected file, or null if no URI can be found from the selection 
 	 */
 	private static URI selectedFileURI(ISelection selection) {
-		if (selection.isEmpty())
+		if (selection.isEmpty()) {
 			return null;
-		
-		if (! (selection instanceof TreeSelection))
+		}
+		if (! (selection instanceof TreeSelection)) {
 			return null;
-		
+		}
 		IStructuredSelection tselection = (IStructuredSelection) selection;
 		Object selectedElement = tselection.getFirstElement();
 		
-		if (! (selectedElement instanceof PlatformObject))
+		if (! (selectedElement instanceof PlatformObject)) {
 			return null;
-		
+		}
 		PlatformObject object = (PlatformObject) selectedElement;
 		IPath path = object.getAdapter(IFile.class).getFullPath();
 		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
@@ -135,16 +163,21 @@ public class LaunchWorkflowShortcut implements ILaunchShortcut {
 	}
 	
 	/**
-	 * Loads an Activity from the given URI.
+	 * Loads an object from the given URI.
 	 * 
 	 * @param uri
 	 * 			The URI corresponding to the Resource to load.
 	 * 
+	 * @param <T>
+	 * 			The exact type of the object to load.
+	 * 
 	 * @return the loaded Activity or {@code null} if the Resource does not contain any
 	 * 
 	 * @throws IOException if an I/O error occurs while loading the Resource
+	 * @throws ClassCastException if the loaded object cannot be casted to T
 	 */
-	private static Activity loadActivity(URI uri) throws IOException {
+	@SuppressWarnings("unchecked")
+	protected static <T> T load(URI uri) throws IOException {
 		ResourceSet resourceSet = new ResourceSetImpl();
 		Resource resource = resourceSet.createResource(uri);
 		resource.load(Collections.emptyMap());
@@ -152,7 +185,7 @@ public class LaunchWorkflowShortcut implements ILaunchShortcut {
 		if (resource.getContents().isEmpty())
 			return null;
 		
-		return (Activity) resource.getContents().get(0);
+		return (T) resource.getContents().get(0);
 	}
 
 }
